355f446e45494ba5f2722e6b10a572216758a187
Seif Yasser <126684081+Seif-Yasser-Ahmed@users.noreply.github.com>
2024-07-27 18:22:17 +0300

    Styling with React

---
+++ Task19/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// import Users from "./components/Users";
+import Users from "../components/Users";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Users></Users>
+    </>
+  );
+}
+
+export default App;
